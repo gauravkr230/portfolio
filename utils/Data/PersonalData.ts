518d2f64f52c268f3246194954dada3b342e32c4
Gaurav kumar <gauravkumar23022006@gmail.com>
2026-04-01 10:30:56 +0530

    Update PersonalData.ts

--- utils/Data/PersonalData.ts
+++ utils/Data/PersonalData.ts
@@ -12,7 +12,7 @@ export const personalData = {
   description:
     "I’m Gaurav kumar, a Full stack Developer focused on building scalable, high-perfomace web applications. I specialize in modern technolgies like React, Next.js, and typeScript , creating seamless and intuitive experiences. With a strong foundation in Data Structures and Algorithms, I approach problems with a solution-driven mindset and write efficient, maintainable code. I enjoy turning complex ideas into real-world products that are fast, reliable and user-friendly. I'm constantly learning and exploring new technologies to stay ahead, with a clear goal of building impactful software and growing as a software Engineer.",
   email: "gauravkumar23022006@gmail.com",
-  phone: "+9153686464",
+  phone: "+919153686464",
   address: "Prayagraj, Uttar Pradesh, India",
   github: "https://github.com/gauravkr230",
   facebook: "https://www.facebook.com/sanam.ban.568/",
